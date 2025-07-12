c436fc10a0664e99cf5bc0490605311346850264
JaiminVaddoriya <jaiminvaddoriya194@gmail.com>
2025-07-12 08:59:14 +0000

    ITEM API changes

--- server/src/controllers/adminControllers/item.controller.js
+++ server/src/controllers/adminControllers/item.controller.js
@@ -2,6 +2,7 @@ const ApiError = require('../../utils/apiError');
 const catchAsync = require('../../utils/catchAsync');
 const itemService = require('../../services/item.service');
 const fileService = require('../../services/files.service');
+const { FILES_FOLDER } = require('../../helper/constant.helper');
 
 /** Update */
 const manageItemStatus = catchAsync(async (req, res) => {
@@ -29,7 +30,9 @@ const deleteItem = catchAsync(async (req, res) => {
     }
 
     // Delete files
-    fileService.deleteFiles(deleteItem.remove_images);
+    fileService.deleteFiles(
+        deleteItem.items.map((image) => `./${FILES_FOLDER.clothImages}/${image}`)
+    );
 
     return res.status(200).json({
         success: true,
--- server/src/models/item.model.js
+++ server/src/models/item.model.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
-const { ITEM } = require('../helper/constant.helper');
+const { ITEM, FILES_FOLDER } = require('../helper/constant.helper');
 const { paginate } = require('./plugins');
+const config = require('../config/config');
 
 const itemSchema = new mongoose.Schema(
     {
@@ -23,6 +24,16 @@ const itemSchema = new mongoose.Schema(
     {
         timestamps: true,
         versionKey: false,
+        toJSON: {
+            transform: function (_doc, ret) {
+                ret.images =
+                    ret.images?.length && !ret.images?.includes('', null)
+                        ? ret.images.map(
+                              (image) => `${config.base_url}/${FILES_FOLDER.clothImages}/${image}`
+                          )
+                        : [];
+            },
+        },
     }
 );
 
